8aa08cbc2055e059ee2fa07cd62b0683988ed733
AmauriBlanco <blancoamauri@gmail.com>
2024-04-17 12:44:43 -0300

    2-3 Finalizando evento de drag and drop

--- src/events/dragCardEvents.js
+++ src/events/dragCardEvents.js
@@ -4,11 +4,32 @@ function dragCardEvents() {
         card.setAttribute("data-card-index", index)
         card.addEventListener('dragstart', dragStart);
     });
+
+    setupDragAndDrop();
 }
 
 function dragStart(event) {
     const cardIndex = event.target.getAttribute('data-card-index');
     event.dataTransfer.setData("text/plain", cardIndex);
+}
+
+function setupDragAndDrop() {
+    const cardsContainer = document.querySelectorAll('.cards-container');
+    cardsContainer.forEach((cardDrop) => {
+        cardDrop.addEventListener('dragover', function(event){
+            event.preventDefault();
+        });
+        dropArea(cardDrop);
+    })
+}
+
+function dropArea(cardDrop) {
+    cardDrop.addEventListener('drop', (event)=> {
+        event.preventDefault();
+        const cardIndex = event.dataTransfer.getData('text/plain');
+        const draggedCard = document.querySelector(`[data-card-index="${cardIndex}"]`);
+        cardDrop.appendChild(draggedCard)
+    })
 }
 
 export { dragCardEvents };
